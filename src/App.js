a79d33941cf2c5f7e4843fa2f0c80b0568495674
robidful <150020689+robidful@users.noreply.github.com>
2024-02-08 16:46:52 +0000

    Meanings implementation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
+        <header className="App-header">Dictionary</header>
         <main>
           <Dictionary />
         </main>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={key}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={keywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
