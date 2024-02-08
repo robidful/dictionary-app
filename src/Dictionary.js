b92cab158cded1a78c597227f3d3ea2363392d07
robidful <150020689+robidful@users.noreply.github.com>
2024-02-08 15:15:31 +0000

    added axios + integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={key}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function keywordChange(event) {
