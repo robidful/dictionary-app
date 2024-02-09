f15bdeb72058c827f25eec8a6222cec46ee7eaad
robidful <150020689+robidful@users.noreply.github.com>
2024-02-09 12:51:57 +0000

    set a default word to show

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,15 +4,15 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>;
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
-              <strong>Example:</strong>
+              <strong>Example: </strong>
               <em>{definition.example}</em>
 
               <Synonyms synonyms={definition.synonyms} />
