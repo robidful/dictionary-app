d51b5a8cfdfe9f2e0011ff923d7f85a45935c3be
robidful <150020689+robidful@users.noreply.github.com>
2024-02-13 11:29:18 +0000

    removed "example" from meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -13,8 +13,6 @@ export default function Meaning(props) {
               <strong>Definition: </strong>
               {definition.definition}
               <br />
-              <strong>Example: </strong>
-              <em>{definition.example}</em>
 
               <Synonyms synonyms={definition.synonyms} />
             </p>
